a3d8ac8401e8a14fdfaf151fc173a987492a3f2b
fa-bula <fa_bula@mail.ru>
2016-07-02 16:35:43 +0300

    Merged faTransform.py and transformAllFa.sh for better understanding

--- bin/faTransform.py
+++ bin/faTransform.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python
+"""Transforms all .fa files in given directory to sequence of
+nucleotides and writes it to output directory"""
 import sys
+import os
+import analyseMutations as am
 
-# Transforms .fa file to sequence of nucleotides
-if len(sys.argv) != 2:
-    print "Usage: {0} chromosome.fa".format(sys.argv[0])
-    sys.exit()
-
-genomeSequence = ''
-with open(sys.argv[1], 'r') as genomeFile:
-        line = genomeFile.readline() # Miss header
+def transform_fa(fa_file):
+    """ Transforms .fa file to sequence of nucleotides"""
+    with open(fa_file) as f:
+        genome_sequence = ''
+        line = f.readline()     # Missing header
         while line != '':
-            line = genomeFile.readline()
-            genomeSequence += line[:-1]
+            line = f.readline()
+            genome_sequence += line[:-1]
+    return genome_sequence
 
-with open(sys.argv[1][:-3], 'w') as outFile:
-    outFile.write(genomeSequence)
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        sys.exit("Usage: {0} faFilesDir outDir".format(sys.argv[0]))
+
+    fa_files = am.get_only_files(sys.argv[1])
+    for file_name in fa_files:
+        out_file_name = os.path.join(sys.argv[2],
+                                   os.path.basename(file_name)[:-3])
+        with open(out_file_name, 'w') as f:
+            f.write(transform_fa(file_name))
