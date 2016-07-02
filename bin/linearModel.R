60a4c232101c706c167309b1954c5235cd0f5433
fa-bula <fa_bula@mail.ru>
2016-07-02 15:42:53 +0300

    changed probability -> frequency delete unnecessary comments swap axes of final plot

--- bin/linearModel.R
+++ bin/linearModel.R
@@ -8,27 +8,27 @@ suppressMessages(library(lmtest))
 args = commandArgs(trailingOnly = TRUE) # Command line arguments
 if (length(args) != 3) {
     scriptName <- "./linearModel.R"
-    sprintf("Usage: %s probabilityDir enrichment.txt outDir", scriptName)
+    sprintf("Usage: %s frequencyDir enrichment.txt outDir", scriptName)
 } else {
-    probabilityDir <- args[1]
+    frequencyDir <- args[1]
     enrichmentFileName <- args[2]
     outDir <- args[3]
 }
 
-probabilityFiles <- list.files(probabilityDir)
+probabilityFiles <- list.files(frequencyDir)
 coeffTable <- data.frame(Sample=character(), Coeff=integer())
 for (fileName in probabilityFiles) {
-    fullPath = file.path(probabilityDir, fileName)
-    probabilityTable <- read.csv(fullPath, encoding="utf-8",
+    fullPath = file.path(frequencyDir, fileName)
+    frequencyTable <- read.csv(fullPath, encoding="utf-8",
                                  header=T, sep='\t')
-    model <- lm(probability ~ replicationTiming, data=probabilityTable)
+    model <- lm(frequency ~ replicationTiming, data=frequencyTable)
     
     png(file.path(outDir, paste(fileName, "png", sep=".")))
-    plot(probabilityTable$replicationTiming,
-         probabilityTable$probability,
+    plot(frequencyTable$replicationTiming,
+         frequencyTable$frequency,
          main = paste("Sample", fileName),
          xlab = "replicationTiming",
-         ylab = "APOBEC mutation probability")
+         ylab = "APOBEC mutation frequency")
     abline(model)
     dev.off()
 
@@ -39,25 +39,18 @@ for (fileName in probabilityFiles) {
 
 enrichmentTable <- read.csv(enrichmentFileName, encoding="utf-8",
                             header=T, sep='\t')
+
 mergedTable <- merge(enrichmentTable, coeffTable, by="Sample")
 ## Very strange sample
 ## TODO: check this sample
-mergedTable <- mergedTable[mergedTable$Sample != "PD4120a",]
-write.table(mergedTable, file=file.path(outDir, "mergedTable"),
-            sep='\t')
+## mergedTable <- mergedTable[mergedTable$Sample != "PD4120a",]
+
 ## finalModel <- lm(Coeff ~ APOBEC_enrich, data=mergedTable)
 png(file.path(outDir, "!finalPlot.png"))
-plot(mergedTable$Coeff,
-     mergedTable$APOBEC_enrich,
+plot(as.numeric(as.character(mergedTable$APOBEC_enrich)),
+     mergedTable$Coeff,
      main = "final plot",
-     xlab = "Coeff",
-     ylab = "APOBEC-enrichment")
+     xlab = "APOBEC-enrichment",
+     ylab = "Coeff")
 ## abline(finalModel)
 dev.off()
-## mod <- lm(formula = mailing ~ orders + previous_mailing)
-## previous <- mailing[length(mailing)]
-## for (order_number in summer_orders) {
-##     previous <- predict(mod, data.frame(orders = c(order_number), previous_mailing = c(previous)))
-##     print(paste(order_number, previous, sep="\t"))
-## }
-
