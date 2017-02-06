7f3490f28d79440c0be8322a602204c05184c801
Bulat <fa_bula@mail.ru>
2017-02-06 11:20:17 +0300

    Added calculation steps and qsub job arrays

--- bin/calculate.py
+++ bin/calculate.py
@@ -2,31 +2,73 @@
 """ Perform all neaded calculations"""
 import sys
 import os
+import pandas
 sys.path.append('/home/fa_bula/diploma/bin/')
 import core
 import mutations
+import motif
+import split
+import frequency
+
+
+def create_if_needed(*dirnames):
+    """ Check are given directories exist and create them,
+    if they not created yet"""
+    if len(dirnames) == 0:
+        return
+    for dir in dirnames:
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
         sys.exit("Usage {0} mutations enrichment".format(sys.argv[0]))
     with open(os.path.join(core.HOME, 'sorted_motifs')) as fin:
-        for line in fin:
-            core.MOTIFS = [line[:-1]]
-            core.INIT_NUCL = [line[1]]
-            for i in core.INIT_NUCL:
-                print i
-            # Create directory for results if needed
-            res_dir = os.path.join(core.HOME, 'results', core.MOTIFS[0])
-            if not os.path.exists(res_dir):
-                os.makedirs(res_dir)
-                
-            # mutations.py part
-            sample_names = mutations.get_sample_names(sys.argv[2])
-            for sample in sample_names:
-                result = mutations.get_mutation_rep_time(sys.argv[1], sample)
-            outFileName = os.path.join(res_dir, sample)
-            with open(outFileName, 'w') as fout:
-                for repTime in result:
-                    fout.write(str(repTime) + '\n')
-            
+        motifs = fin.readlines()
+    print(int(os.environ['SGE_TASK_ID']))
+    core.MOTIFS = [motifs[int(os.environ['SGE_TASK_ID'])][:-1]]
+    core.INIT_NUCL = [m[1] for m in core.MOTIFS]
+    print "Considering motif {0}, initial nucleotide {1} and final nucleotide {2}".format(core.MOTIFS[0], core.INIT_NUCL[0], core.FINAL_NUCL[0])
+
+    res_dir = os.path.join(core.HOME, 'results', core.MOTIFS[0])
+    mut_dir = os.path.join(res_dir, 'mutation_rep_time')
+    gen_dir = os.path.join(res_dir, 'genome_rep_time')
+    freq_dir = os.path.join(res_dir, 'mutations_frequency')
+    gen_bins = os.path.join(res_dir, 'genome_rep_time_bins')
+    create_if_needed(res_dir, mut_dir, gen_dir, freq_dir)
+
+    # mutations.py part
+    sample_names = mutations.get_sample_names(sys.argv[2])
+    for sample in sample_names:
+        print "Sample {0}\n".format(sample)
+        result = mutations.get_mutation_rep_time(sys.argv[1], sample)
+        outFileName = os.path.join(mut_dir, sample)
+        with open(outFileName, 'w') as fout:
+            for repTime in result:
+                fout.write(str(repTime) + '\n')
     
+    # motif.py part
+    genome_file_names = core.get_genome_file_names()
+    for chromosome in genome_file_names:
+        motifReplicationTiming = motif.get_motif_rep_time(chromosome)
+        print "Chromosome #{0} motifs has been considered\n".format(chromosome)
+        outFileName = os.path.join(gen_dir, chromosome)
+        with open(outFileName, 'w') as fout:
+            for time in motifReplicationTiming:
+                fout.write(str(time) + '\n')
+            del motifReplicationTiming
+    
+    # split.py part
+    split.calculate_borders(gen_dir, gen_bins)
+    
+    # frequency.py part
+    motifs_in_bin = pandas.read_csv(gen_bins, sep='\t')
+    mutation_rep_time_files = core.get_only_files(mut_dir)
+    for mutation_file in mutation_rep_time_files:
+        with open(mutation_file) as f:
+            replication_timings = map(float, f)
+            freq = frequency.calculate_frequency(motifs_in_bin,
+                                                 replication_timings)
+            outFile = os.path.join(freq_dir, os.path.basename(mutation_file))
+            freq.to_csv(outFile, sep='\t')
--- bin/run.sh
+++ bin/run.sh
@@ -1,16 +1,20 @@
+# Script to perform all needed calculations and wait for it finish
 # Remove old .log files
 rm ../logs/errors.log
 rm ../logs/output.log
+rm ../logs/exec_time
 
 # Run program with specified destination of errors and output
-qsub -cwd -S /usr/bin/python -e ../logs/errors.log -o ../logs/output.log calculate.py ../breast/mutations.txt ../breast/enrichment
+clear
+date > ../logs/exec_time	# Write start time at file
+qsub -t 1-63 -cwd -S /usr/bin/python -e ../logs/errors.log -o ../logs/output.log calculate.py ../breast/mutations.txt ../breast/enrichment
 
 # Wait for qsub exit and then show output.log and errors.log
 OUTPUT="$(qstat)"
 while [ -n "$OUTPUT" ]; do
-    sleep 5
+    sleep 5 # Wait 5 sec and then check if calculations finished
     OUTPUT="$(qstat)"
 done
-echo "Finished!"
+echo "Calculations has been finished!"
+date >> ../logs/exec_time	# Append end time to file
 less -e ../logs/errors.log ../logs/output.log 
-
