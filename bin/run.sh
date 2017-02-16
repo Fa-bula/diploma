1974a68b547da15539d8eb351f84ebfb021846b0
Bulat <fa_bula@mail.ru>
2017-02-16 18:03:44 +0300

    Added checking newline at the end of file with motifs and fixed log write mechanism

--- bin/run.sh
+++ bin/run.sh
@@ -1,20 +1,21 @@
 # Script to perform all needed calculations and wait for it finish
 # Remove old .log files
-rm ../logs/errors.log
-rm ../logs/output.log
-rm ../logs/exec_time
-
+rm ../logs/*
+# motifs = ../
 # Run program with specified destination of errors and output
 clear
 date > ../logs/exec_time	# Write start time at file
-qsub -t 1-63 -cwd -S /usr/bin/python -e ../logs/errors.log -o ../logs/output.log calculate.py ../breast/mutations.txt ../breast/enrichment
 
+test `tail -c 1 ../sorted_motifs` && { echo "Error: ../sorted_motifs should end with newline"; exit 1; }
+a=$((`wc -l ../sorted_motifs | awk '{print $1;}'`-1)) # Number of motifs in file
+
+qsub -t 1-${a} -cwd -S /usr/bin/python -e ../logs/ -o ../logs/ calculate.py ../breast/mutations.txt ../breast/enrichment
 # Wait for qsub exit and then show output.log and errors.log
 OUTPUT="$(qstat)"
 while [ -n "$OUTPUT" ]; do
     sleep 5 # Wait 5 sec and then check if calculations finished
     OUTPUT="$(qstat)"
 done
-echo "Calculations has been finished!"
+echo "Calculations, job has been finished!"
 date >> ../logs/exec_time	# Append end time to file
-less -e ../logs/errors.log ../logs/output.log 
+cat ../logs/exec_time
