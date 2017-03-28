4ce90a58b4bf01f95c7609a8510e50411c11ceb8
Bulat <fa_bula@mail.ru>
2017-03-28 20:03:56 +0300

    Generate signarure complementary pairs instead of lonely signatures

--- bin/generate.py
+++ bin/generate.py
@@ -2,6 +2,8 @@
 """ Generate file with mutation signature pairs. Signatures in pairs are complementary"""
 import itertools
 import sys
+sys.path.append('/home/fa_bula/diploma/bin/')
+import core
 
 complementary_dict = {
     'A': 'T',
@@ -10,19 +12,19 @@ complementary_dict = {
     'G': 'C'}
 
 def get_complementary(motif):
+    """ Returns complementary motif"""
+    # Replace nucleotedes with complementary
     substituted = [complementary_dict[i] for i in motif]
+    # Reverse nucleotide sequence
     complementary = list(reversed(substituted))
     return complementary
 
 
-if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        sys.exit("Usage {0} motif_length output_file".format(sys.argv[0]))
-    length = int(sys.argv[1])
-    
+def generate_signatures(motif_length):
+    """ Creates list of all distinct complementary mutation signatures"""
     # Generate (motif, complementary_motif) for all possible motifs
     pairs = [(''.join(i), ''.join(get_complementary(i))) for i in itertools.product("ACGT",
-                                                                           repeat = length)]
+                                                                                    repeat = motif_length)]
     # Sort pairs by minimum of two motifs to remove duplicates
     sorted_pairs = sorted(pairs, key = lambda x: min(x[0], x[1]))
     unique_pairs = sorted_pairs[1::2]
@@ -30,9 +32,21 @@ if __name__ == '__main__':
     signatures = []
     for nucl in complementary_dict:
         for pair in unique_pairs:
-            if pair[0][length / 2] != nucl:
-                signatures.append((pair[0], nucl, pair[1], complementary_dict[nucl]))
+            if pair[0][motif_length / 2] != nucl:
+                first = core.Mutation_Signature((pair[0], nucl), 'subs')
+                second = core.Mutation_Signature((pair[1], complementary_dict[nucl]), 'subs')
+                signatures.append((first, second))
+                                   
+    return signatures
                 
+    
+if __name__ == '__main__':
+    # Use if you want to check, what signatures created
+    if len(sys.argv) != 3:
+        sys.exit("Usage {0} motif_length output_file".format(sys.argv[0]))
+    length = int(sys.argv[1])
+    
+    signatures = generate_signatures(length)
     with open(sys.argv[2], "w") as fout:
         for p in signatures:
-            fout.write("{0} {1} {2} {3}\n".format(p[0], p[1], p[2], p[3]))
+            fout.write("{0} {1}\n".format(p[0], p[1]))
