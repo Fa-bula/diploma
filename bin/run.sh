2ccbbf0327d59c8db4abdd2d6a376bb495356a83
Bulat <fa_bula@mail.ru>
2017-03-28 20:01:10 +0300

    Runnig linearModel.R and spliting works to 10 parts to deal with memory lack

--- bin/run.sh
+++ bin/run.sh
@@ -1,20 +1,27 @@
 # Script to perform all needed calculations and wait for it finish
 # Remove old .log files
 rm ../logs/*
-# motifs = ../
+
 # Run program with specified destination of errors and output
 clear
 date > ../logs/exec_time	# Write start time at file
+# Python part
+# FIXME: maybe loop?
+qsub -t 1-9 -sync y -cwd -S /usr/bin/python -e ../logs/ -o ../logs/ calculate.py ../breast/mutations.txt ../breast/enrichment ../breast_results
+qsub -t 10-19 -sync y -cwd -S /usr/bin/python -e ../logs/ -o ../logs/ calculate.py ../breast/mutations.txt ../breast/enrichment ../breast_results
+qsub -t 20-29 -sync y -cwd -S /usr/bin/python -e ../logs/ -o ../logs/ calculate.py ../breast/mutations.txt ../breast/enrichment ../breast_results
+qsub -t 30-39 -sync y -cwd -S /usr/bin/python -e ../logs/ -o ../logs/ calculate.py ../breast/mutations.txt ../breast/enrichment ../breast_results
+qsub -t 40-49 -sync y -cwd -S /usr/bin/python -e ../logs/ -o ../logs/ calculate.py ../breast/mutations.txt ../breast/enrichment ../breast_results
+qsub -t 50-59 -sync y -cwd -S /usr/bin/python -e ../logs/ -o ../logs/ calculate.py ../breast/mutations.txt ../breast/enrichment ../breast_results
+qsub -t 60-69 -sync y -cwd -S /usr/bin/python -e ../logs/ -o ../logs/ calculate.py ../breast/mutations.txt ../breast/enrichment ../breast_results
+qsub -t 70-79 -sync y -cwd -S /usr/bin/python -e ../logs/ -o ../logs/ calculate.py ../breast/mutations.txt ../breast/enrichment ../breast_results
+qsub -t 80-89 -sync y -cwd -S /usr/bin/python -e ../logs/ -o ../logs/ calculate.py ../breast/mutations.txt ../breast/enrichment ../breast_results
+qsub -t 90-96 -sync y -cwd -S /usr/bin/python -e ../logs/ -o ../logs/ calculate.py ../breast/mutations.txt ../breast/enrichment ../breast_results
 
-test `tail -c 1 ../sorted_motifs` && { echo "Error: ../sorted_motifs should end with newline"; exit 1; }
-a=$((`wc -l ../sorted_motifs | awk '{print $1;}'`-1)) # Number of motifs in file
-
-qsub -t 1-${a} -cwd -S /usr/bin/python -e ../logs/ -o ../logs/ calculate.py ../breast/mutations.txt ../breast/enrichment
-# Wait for qsub exit and then show output.log and errors.log
-OUTPUT="$(qstat)"
-while [ -n "$OUTPUT" ]; do
-    sleep 5 # Wait 5 sec and then check if calculations finished
-    OUTPUT="$(qstat)"
+# R part
+RESULTS=../breast_results/*/mutations_frequency/
+for dir in $RESULTS; do
+    qsub  -cwd -S /usr/bin/Rscript -e ../logs/ -o ../logs/ linearModel.R $dir ../breast/enrichment ../plots $(basename $(dirname $dir))
 done
 echo "Calculations, job has been finished!"
 date >> ../logs/exec_time	# Append end time to file
