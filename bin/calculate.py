64a1fd368e5ad98d3af002448a11300058176cdb
Bulat <fa_bula@mail.ru>
2017-03-29 13:00:31 +0300

    Changes due to using MutationSignature class and generate.py invocation added

--- bin/calculate.py
+++ bin/calculate.py
@@ -9,6 +9,7 @@ import mutations
 import motif
 import split
 import frequency
+import generate
 
 
 def create_if_needed(*dirnames):
@@ -22,16 +23,18 @@ def create_if_needed(*dirnames):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        sys.exit("Usage {0} mutations enrichment".format(sys.argv[0]))
-    with open(os.path.join(core.HOME, 'sorted_motifs')) as fin:
-        motifs = fin.readlines()
-    print(int(os.environ['SGE_TASK_ID']))
-    core.MOTIFS = [motifs[int(os.environ['SGE_TASK_ID'])][:-1]]
-    core.INIT_NUCL = [m[1] for m in core.MOTIFS]
-    print "Considering motif {0}, initial nucleotide {1} and final nucleotide {2}".format(core.MOTIFS[0], core.INIT_NUCL[0], core.FINAL_NUCL[0])
+    if len(sys.argv) != 4:
+        sys.exit("Usage {0} mutations enrichment results_dir".format(sys.argv[0]))
 
-    res_dir = os.path.join(core.HOME, 'results', core.MOTIFS[0])
+    # generate.py part
+    signature_pairs = generate.generate_signatures(3)
+    
+    # Take pairs for this thread only
+    signatures = signature_pairs[int(os.environ['SGE_TASK_ID']) - 1] # Cuz SGE_TASK_ID begins with 1
+    print "Considering {0} mutation signatures".format(signatures)
+
+    # Creating result subdirecories for curren
+    res_dir = os.path.join(sys.argv[3], "{0}+{1}".format(signatures[0], signatures[1]))
     mut_dir = os.path.join(res_dir, 'mutation_rep_time')
     gen_dir = os.path.join(res_dir, 'genome_rep_time')
     freq_dir = os.path.join(res_dir, 'mutations_frequency')
@@ -42,7 +45,7 @@ if __name__ == '__main__':
     sample_names = mutations.get_sample_names(sys.argv[2])
     for sample in sample_names:
         print "Sample {0}\n".format(sample)
-        result = mutations.get_mutation_rep_time(sys.argv[1], sample)
+        result = mutations.get_mutation_rep_time(sys.argv[1], sample, signatures)
         outFileName = os.path.join(mut_dir, sample)
         with open(outFileName, 'w') as fout:
             for repTime in result:
@@ -51,7 +54,7 @@ if __name__ == '__main__':
     # motif.py part
     genome_file_names = core.get_genome_file_names()
     for chromosome in genome_file_names:
-        motifReplicationTiming = motif.get_motif_rep_time(chromosome)
+        motifReplicationTiming = motif.get_motif_rep_time(chromosome, signatures)
         print "Chromosome #{0} motifs has been considered\n".format(chromosome)
         outFileName = os.path.join(gen_dir, chromosome)
         with open(outFileName, 'w') as fout:
